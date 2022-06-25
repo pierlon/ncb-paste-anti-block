562ad7d562fafc6b15635633f684cfd4f34ab855
Pierre Gordon <pierregordon@protonmail.com>
2022-06-25 00:59:28 -0400

    Add autofill support

--- content.js
+++ content.js
@@ -1,7 +1,25 @@
-const input = document.getElementById('AuthenticationFG.ACCESS_CODE')
+const form = document.querySelector('form[name="AuthenticationFG"]')
 
-if (input) {
-    input.addEventListener('paste', (e) => {
-        e.stopImmediatePropagation()
-    })
+if (form) {
+    const inputName = document.getElementById('AuthenticationFG.USER_PRINCIPAL')
+    const inputPass = document.getElementById('AuthenticationFG.ACCESS_CODE')
+
+    if (inputName && !inputPass) {
+        inputName.addEventListener('paste', (e) => {
+            e.stopImmediatePropagation()
+        })
+
+        inputName.setAttribute('autocomplete', 'username')
+    }
+
+    if (inputPass) {
+        inputPass.addEventListener('paste', (e) => {
+            e.stopImmediatePropagation()
+        })
+
+        inputPass.setAttribute('autocomplete', 'password')
+    }
+    
+    const inputDummies = Array.from(document.querySelectorAll('input:not([required])[type="password"]'))
+    inputDummies.forEach(dummy => dummy.setAttribute('type', 'hidden'))
 }
